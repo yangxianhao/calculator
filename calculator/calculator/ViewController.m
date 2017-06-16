f5de837943acdae169bc9b6ffa459b0ca38fb68e
杨先豪 <xianhao.yang@bluechips.net.cn>
2017-06-16 13:19:18 +0800

    输入数据控制

--- calculator/calculator/ViewController.m
+++ calculator/calculator/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+
 @end
 
 @implementation ViewController
@@ -19,6 +21,92 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (IBAction)zero {
+    if ([self.resultLabel.text isEqualToString:@"0"]) return;
+    [self appendNumber:@"0"];
+}
+
+- (IBAction)one {
+    [self appendNumber:@"1"];
+}
+
+- (IBAction)two {
+    [self appendNumber:@"2"];
+}
+
+- (IBAction)three {
+    [self appendNumber:@"3"];
+}
+
+- (IBAction)four {
+    [self appendNumber:@"4"];
+}
+
+- (IBAction)five {
+    [self appendNumber:@"5"];
+}
+
+- (IBAction)six {
+    [self appendNumber:@"6"];
+}
+
+- (IBAction)seven {
+    [self appendNumber:@"7"];
+}
+
+- (IBAction)eight {
+    [self appendNumber:@"8"];
+}
+
+- (IBAction)nine {
+    [self appendNumber:@"9"];
+}
+
+- (IBAction)point {
+    [self appendNumber:@"."];
+}
+
+- (IBAction)add {
+    
+}
+
+- (IBAction)minus {
+    
+}
+
+- (IBAction)times {
+    
+}
+
+- (IBAction)divide {
+    
+}
+
+- (IBAction)equal {
+    
+}
+
+- (IBAction)clear {
+    self.resultLabel.text = @"0";
+}
+
+- (IBAction)opposite {
+    
+}
+
+- (IBAction)percentage {
+    
+}
+
+- (void)appendNumber:(NSString *)number
+{
+    if ([self.resultLabel.text isEqualToString:@"0"] && ![number isEqualToString:@"."]) {
+        self.resultLabel.text = @"";
+    }
+    NSString *tempStr = self.resultLabel.text;
+    self.resultLabel.text = [tempStr stringByAppendingString:number];
+    
+}
 
 - (UIStatusBarStyle)preferredStatusBarStyle
 {

2e6fd1d37ee2b331cd0605ce72990f88e6ec2377
xianhaoyang <xianhao.yang@bluechips.net.cn>
2017-06-24 16:23:35 +0800

    加入判断中文分类

---
+++ calculator/calculator/NSString+Chinese.h
@@ -0,0 +1,17 @@
+//
+//  NSString+Chinese.h
+//  calculator
+//
+//  Created by 杨先豪 on 2017/6/24.
+//  Copyright © 2017年 yangxianhao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Chinese)
+
+- (BOOL)isChinese;//判断是否是纯汉字
+
+- (BOOL)includeChinese;//判断是否含有汉字
+
+@end
